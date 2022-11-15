bda0e24988ae9d93d940c72cc92216a7a0e7f87a
Medha Moorching <mepiper2010@gmail.com>
2022-11-15 14:04:19 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import React from "react";
+import Confetti from "react-confetti"
+import useWindowSize from 'react-use/lib/useWindowSize'
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState()
+  const [luckyNum, setLuckyNum] = useState()
+  const [output, setOutput] = useState("")
+  const [lucky, setLucky] = useState(false)
+  const { width, height } = useWindowSize()
+ 
+
+  const calculateSum =(date) =>{
+    let dateInt = date.replaceAll("-","")
+    let sumDate = 0
+    for(let i=0; i<dateInt.length; i++){
+      sumDate+=Number (dateInt.charAt(i))
+    }
+
+    checkIfLucky(sumDate)
+
+  }
+
+  
+
+  const checkIfLucky=(sumDate)=>{
+    
+    
+    if(sumDate % luckyNum === 0){
+      setLucky(true)
+      setOutput("🎉Your birthday is lucky! 🎉")}
+    else{
+      setLucky(false)
+      setOutput("Your birthday is not so lucky ☹️")
+      
+    }
+  }
+
+  const handleSubmit=()=>{
+    calculateSum(date)
+   
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <h1>Is your birthday lucky?🍀</h1>
+   
+    <p className="intro"> Check if your birthday is lucky or not based on your lucky number!</p>
+    <div className='container'>
+    <label htmlFor='date-of-birth'>Date of Birth:</label><br/>
+    <input  type='date' id='date-of-birth' onChange={(e)=>setDate(e.target.value)}/><br/>
+    <label htmlFor='lucky-number'>Enter your lucky Number</label><br/>
+    <input id='lucky-number' type='number' onChange={(e)=>{setLuckyNum(e.target.value)}}/><br/>
+    <button className="btn" onClick={()=>handleSubmit()}>Check Number</button><br /><br/>
+    <div className='output'>{output} </div>
+    {lucky && 
+    <Confetti 
+        width={width}
+        height={height}
+      />}
+    
+   </div>
+   </>
   );
 }
 
